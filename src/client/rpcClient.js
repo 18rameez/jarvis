4c223670845910121689ed5970ef3f219944afe8
18rameez <18rameez@gmail.com>
2024-09-07 17:46:00 +0530

    fix: daemon launching issue

--- src/client/rpcClient.js
+++ src/client/rpcClient.js
@@ -10,20 +10,33 @@ req.connect(port);
 
 // Function to ensure the daemon is running
 function ensureDaemonIsRunning(launchDaemon, callback) {
-    launchDaemon(() => {});
-    client.call('ping', null, function(err, isRunning) {
-
-        if (err || !isRunning) {
-            console.log('Daemon not launched. Starting daemon...');
-            launchDaemon(() => {
-                console.log('Daemon started. Retrying command...');
-                callback();
-            });
-        } else {
+    // Schedule a timeout to call launchDaemon after 3 seconds. This ensures that if the ping fails (because the daemon is not running),
+    // the daemon will be started automatically after the delay.
+    const timeoutId = setTimeout(() => {
+        console.log('Starting daemon after 3 seconds...');
+        launchDaemon(() => {
+            console.log('Daemon started. Retrying command...');
             callback();
-        }
-    });
+        });
+    }, 2000);
+
+    try {
+        // Attempt to ping the daemon
+        client.call('ping', null, function (err, isRunning) {
+            if (err || !isRunning) {
+                console.log('Daemon not launched. Waiting for 3 seconds before starting...');
+                // The setTimeout will automatically start the daemon after 3 seconds
+            } else {
+                console.log('Daemon is already running.');
+                clearTimeout(timeoutId);
+                callback();
+            }
+        });
+    } catch (error) {
+        console.error('Error occurred while pinging daemon:', error);
+    }
 }
+
 
 // Export the client and related functions
 module.exports = {
